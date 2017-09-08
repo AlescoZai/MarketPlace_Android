abb85568e92683a14a66068bc750588a8c0826dd
dennismuturia <dmuturia2922@gmail.com>
2017-09-08 11:44:58 +0300

    My what to buy was not displaying
    So the problem was my manifest file. When you declare the new class you have to also modify the manifest file
    
    Also added the parent child property on the app the thingy.  The menu does not show on the launch but shows on the what to buy.

--- app/src/main/java/com/example/dennis/marketplace/MainActivity.java
+++ app/src/main/java/com/example/dennis/marketplace/MainActivity.java
@@ -1,31 +1,46 @@
 package com.example.dennis.marketplace;
 
+import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     //Lets declare some variables
-    public TextView myText;
 
     //This is butterknife
-@Bind(R.id.btn)
-    Button myButton;
-    @Bind(R.id.myText)
-    EditText myEditText;
+    @Bind(R.id.btn) Button myButton;
+    @Bind(R.id.myname) EditText myEditText;
+    @Bind(R.id.thisText) TextView myText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //IMplement butterknife
+        ButterKnife.bind(this);
 
         //Lets add a new font to make our app awesome
         myText = (TextView) findViewById(R.id.thisText);
-        Typeface smiling = Typeface.DEFAULT.createFromAsset(getAssets(),"fonts/Laughing and Smiling.ttf");
+        Typeface smiling = Typeface.createFromAsset(getAssets(),"fonts/Laughing and Smiling.ttf");
         myText.setTypeface(smiling);
+
+        //Set an onclick listener
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = myEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, WhatToBuy.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/dennis/marketplace/WhatToBuy.java
@@ -0,0 +1,40 @@
+package com.example.dennis.marketplace;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by dennis on 9/8/17.
+ */
+
+public class WhatToBuy extends AppCompatActivity{
+    public static final String TAG = WhatToBuy.class.getSimpleName();
+//Lets Create some butterknife
+    @Bind(R.id.myText) TextView thisText;
+    @Bind(R.id.thisList) ListView myList;
+    private String[] BucketList = new String[] {"Bread", "Tusker",
+            "Guiness", "Cakes", "Delamere", "BasketBall",
+            "Trousers", "Jackets", "Toothpaste", "Laptops",
+            "Mousse", "FUll Grid", "Glasses",
+            "Muturas", "Subway footlongs"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.whattobuy_activity);
+
+        //Initialize the butterknife
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        thisText.setText(name);
+    }
+}
