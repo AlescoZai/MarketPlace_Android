1dbe8ee269d5a557189f8cf46481b0a82d034bf3
dennismuturia <dmuturia2922@gmail.com>
2017-09-15 16:39:01 +0300

    Added a new empty activity
    This Activity will be the one that will display the content straight after you click it

---
+++ app/src/main/java/com/example/dennis/marketplace/ui/MarketDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.dennis.marketplace.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dennis.marketplace.R;
+
+public class MarketDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market_detail);
+    }
+}
