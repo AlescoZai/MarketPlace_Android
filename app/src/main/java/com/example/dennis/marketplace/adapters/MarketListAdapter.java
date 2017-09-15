c691111f4f5deacf628981197f82da51993b1b14
dennismuturia <dmuturia2922@gmail.com>
2017-09-15 15:45:52 +0300

    Added a list Adapter
    THis is a recycler view that will keep on addingcontinous data and save our memory

---
+++ app/src/main/java/com/example/dennis/marketplace/adapters/MarketListAdapter.java
@@ -0,0 +1,74 @@
+package com.example.dennis.marketplace.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.dennis.marketplace.R;
+import com.example.dennis.marketplace.models.Market;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by dennis on 9/15/17.
+ */
+
+public class MarketListAdapter extends RecyclerView.Adapter<MarketListAdapter.MarketViewHolder>  {
+    private ArrayList<Market> mMarket = new ArrayList<>();
+    private Context mContext;
+
+    //Now lets create a constructor
+    public MarketListAdapter (Context context, ArrayList<Market> markets){
+        mContext = context;
+        mMarket = markets;
+    }
+
+    @Override
+    public MarketListAdapter.MarketViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_item, parent, false);
+        MarketViewHolder viewHolder = new MarketViewHolder(view);
+        return viewHolder;
+    }
+
+    //This is an onbind view
+    @Override
+    public void onBindViewHolder(MarketListAdapter.MarketViewHolder holder, int position) {
+        holder.bindMarket(mMarket.get(position));
+    }
+    //get the amount of items here
+    @Override
+    public int getItemCount() {
+        return mMarket.size();
+    }
+
+
+    public class MarketViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.itemImageView)
+        ImageView mImageView;
+        @Bind(R.id.itemNameTextView) TextView mNameTextView;
+        @Bind(R.id.priceTextView)
+        TextView mPriceTextView;
+        private Context mContext;
+
+        public MarketViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindMarket(Market market) {
+            mNameTextView.setText(market.getName());
+            Picasso.with(mContext).load(market.getImage()).into(mImageView);
+            mPriceTextView.setText("Amount KES: " + market.getSalePrice() + "/100");
+        }
+    }
+
+}
