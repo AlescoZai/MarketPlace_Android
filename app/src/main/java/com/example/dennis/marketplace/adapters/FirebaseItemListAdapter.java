72fca9cd03ec14b9eb3102d4b92d909565e283a9
dennismuturia <dmuturia2922@gmail.com>
2017-09-29 12:16:18 +0300

    Added a firebaseitemlistadapter
    The work of this will be basically to popularte our viewholder in the recyclerview. And also store the position of each view

--- app/src/main/java/com/example/dennis/marketplace/adapters/FirebaseItemListAdapter.java
+++ app/src/main/java/com/example/dennis/marketplace/adapters/FirebaseItemListAdapter.java
@@ -1,8 +1,45 @@
 package com.example.dennis.marketplace.adapters;
 
+import android.content.Context;
+
+import com.example.dennis.marketplace.models.Market;
+import com.example.dennis.marketplace.util.ItemTouchHelperAdapter;
+import com.example.dennis.marketplace.util.OnStartDragListener;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
 /**
  * Created by dennis on 9/29/17.
  */
 
-public class FirebaseItemListAdapter {
+public class FirebaseItemListAdapter extends FirebaseRecyclerAdapter<Market, FirebaseItemViewHolder> implements ItemTouchHelperAdapter {
+    private DatabaseReference mRef;
+    private OnStartDragListener mOnStartDragListener;
+    private Context mContext;
+
+//Create a constructor for this class
+    public FirebaseItemListAdapter(Class<Market> modelClass, int modelLayout,
+                                         Class<FirebaseItemViewHolder> viewHolderClass,
+                                         Query ref, OnStartDragListener onStartDragListener, Context context) {
+        super(modelClass, modelLayout, viewHolderClass, ref);
+        mRef = ref.getRef();
+        mOnStartDragListener = onStartDragListener;
+        mContext = context;
+    }
+//This stores  our X and Y coordinates on the position of our drag
+    @Override
+    public boolean onItemMove(int fromPosition, int toPosition) {
+        return false;
+    }
+
+    @Override
+    public void onItemDismiss(int position) {
+
+    }
+//This will be the one to populate our view holder
+    @Override
+    protected void populateViewHolder(FirebaseItemViewHolder viewHolder, Market model, int position) {
+        viewHolder.bindItems(model);
+    }
 }
