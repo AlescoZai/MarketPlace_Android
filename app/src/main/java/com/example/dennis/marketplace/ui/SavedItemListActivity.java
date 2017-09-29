3c4eca8b4d8442782a952d2a6e278d230903ff1e
dennismuturia <dmuturia2922@gmail.com>
2017-09-29 12:32:32 +0300

    I have updated my saveditem activity
    i added the ItemTouchHelper as a member variable to that we can use it in the OnStartDragListener's onStartDrag() method.
    
    In the FirebaseRestaurantListAdapter constructor, this refers to the OnStartDragListener and the Context. Both are necessary to construct an instance of a FirebaseRestaurantListAdapter.
    
    The SimpleItemTouchHelperCallback takes an adapter as a parameter so we pass it the instance of the FirebaseRestaurantListAdapter i had just created.
    
    The ItemTouchHelper takes a ItemTouchHelper.Callback as an argument so i can pass it the instance of the SimpleItemTouchHelperCallback that i had just created using our adapter.
    
    To enable the interfaces to communicate with the necessary callbacks, i must attach the ItemTouchHelper to our RecyclerView using the attachToRecyclerView() method.
    
    Finally, i call the startDrag() method on the instance of our ItemTouchHelper inside of the onStartDrag() override which will eventually send our touch events back to our SimpleItemTouchHelperCallback.

--- app/src/main/java/com/example/dennis/marketplace/ui/SavedItemListActivity.java
+++ app/src/main/java/com/example/dennis/marketplace/ui/SavedItemListActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import com.example.dennis.marketplace.Constants;
 import com.example.dennis.marketplace.R;
+import com.example.dennis.marketplace.adapters.FirebaseItemListAdapter;
 import com.example.dennis.marketplace.adapters.FirebaseItemViewHolder;
 import com.example.dennis.marketplace.models.Market;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.example.dennis.marketplace.util.OnStartDragListener;
+import com.example.dennis.marketplace.util.SimpleItemTouchHelperCallback;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -18,9 +21,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class SavedItemListActivity extends AppCompatActivity {
+public class SavedItemListActivity extends AppCompatActivity implements OnStartDragListener {
     private DatabaseReference mMarketReference;
-    private FirebaseRecyclerAdapter mFirebaseAdapter;
+    //private FirebaseRecyclerAdapter mFirebaseAdapter;
+    private FirebaseItemListAdapter mFirebaseAdapter;
+    private ItemTouchHelper mItemTouchHelper;
 
     @Bind(R.id.recyclerView)
     RecyclerView mRecyclerView;
@@ -44,9 +49,17 @@ public class SavedItemListActivity extends AppCompatActivity {
     }
 
     private void setUpFirebaseAdapter() {
-        mFirebaseAdapter = new FirebaseRecyclerAdapter<Market, FirebaseItemViewHolder>
-                (Market.class, R.layout.item_list_item_drag, FirebaseItemViewHolder.class,
-                        mMarketReference){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        mMarketReference = FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_CHILD_ITEM)
+                .child(uid);
+
+
+        mFirebaseAdapter = new FirebaseItemListAdapter(Market.class, R.layout.item_list_item_drag, FirebaseItemViewHolder.class,
+                mMarketReference, this, this) {
 
             @Override
             protected void populateViewHolder(FirebaseItemViewHolder viewHolder,
@@ -57,12 +70,20 @@ public class SavedItemListActivity extends AppCompatActivity {
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mFirebaseAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mFirebaseAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
     }
-    /*
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         mFirebaseAdapter.cleanup();
     }
-    */
+
 }
